e89be0ee6eb2061e32e5c4d07b7e35a8c1755c1e
Anthony Guirges <anthony.g.guirges@gmail.com>
2022-07-25 17:36:17 -0700

    multi page demo

---
+++ src/press.js
@@ -0,0 +1,12 @@
+const App = () => {
+    return (
+        <Template>
+            <h1>Press page 1</h1>
+        </Template>
+    );
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
